7c5d86c91b2d8bfe257fb999cc43b77d1cab0c45
gebretewodros73 <gebretewodros73@gmail.com>
2023-02-22 03:33:12 +0000

    modifying existing files

--- shell.c
+++ shell.c
@@ -1,51 +1,44 @@
 #include "shell.h"
-#define MAX_CMD_LEN 1024
+
 /**
- * main - perform the task
- * @argc: counts arg
- * @argv: storse arg
- * Return: Always 0
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	char *command = NULL, *stkn;
-	size_t len = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	while (1)
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
 	{
-		printf("$ ");
-		getline(&command, &len, stdin);
-		stkn = strtok(command, " \n");
-		char **arr = malloc(sizeof(char *) * (MAX_ARGS + 1));
-
-		arr[0] = stkn;
-		int i = 1;
-
-		while (stkn != NULL)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			stkn = strtok(NULL, " \n");
-			arr[i] = stkn;
-			i++; }
-		arr[i] = NULL;
-		if (arr[0] == NULL)
-		{
-			free(arr);
-			continue;
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		if (strcmp(arr[0], "exit") == 0 && (arr[1] == NULL))
-		{
-			printf("Exiting shell...\n");
-			exit(0);
-		}
-		else if (strcmp(arr[0], "env") == 0 && (arr[1] == NULL))
-		{
-			print_environment();
-		}
-		else
-		{
-			execute_run(arr);
-		}
-		free(arr); }
-	free(command);
-	return (0);
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
